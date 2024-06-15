d19389edf4e28b57ff6ee62a6d65881c9dfd5e0f
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 11:29:59 -0300

    Create exe011.py

---
+++ Exercícios/exe011.py
@@ -0,0 +1,4 @@
+comprimento = float(input('Insira o comprimento da parede que deseja pintar: '))
+altura = float(input('Agora insira sua altura: '))
+tinta = (comprimento * altura) / 2
+print('O total de tinta para pintar essa parede são {} litros.'.format(tinta))
