5ee0a0326c8ebe8c11dc26dd8baee9d8075841d8
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 11:35:05 -0300

    Create exe013.py

---
+++ Exercícios/exe013.py
@@ -0,0 +1,3 @@
+salario = float(input('Insira o valor de seu salário: '))
+aumento = salario*0.15
+print('Seu salário atual é de {} reais, com o aumento de {} reais, seu novo salário será de {} reais.'.format(salario, aumento, salario+aumento))
