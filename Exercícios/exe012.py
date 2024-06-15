c04f31b2387e8ace31b4751b636b87f26d93f311
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 14:07:29 -0300

    Update exe012.py

--- Exercícios/exe012.py
+++ Exercícios/exe012.py
@@ -1,3 +1,3 @@
 valor = float(input('Insira o valor do produto que deseja comprar: '))
-preco = valor*0.95
+preco = valor * 0.95
 print('A loja está com 5 por cento de desconto, o preço final será de {} reais.'.format(preco))
