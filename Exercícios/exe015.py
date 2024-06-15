5aede3a50724b117c41a5b808ec261c177447a55
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 11:46:42 -0300

    Create exe015.py

---
+++ Exercícios/exe015.py
@@ -0,0 +1,4 @@
+km = int(input('Insira quantos quilometros foram rodados com esse carro: '))
+dias = int(input('Por quantos dias o carro foi alugado? '))
+valor = km*0.15+dias*60
+print('O valor final do aluguel é de {} reais.'.format(valor))
