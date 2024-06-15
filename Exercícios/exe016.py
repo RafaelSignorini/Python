719d4f499788fdb4467ccae7cca7571abff6c5b5
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 18:38:26 -0300

    Create exe016.py

---
+++ Exercícios/exe016.py
@@ -0,0 +1,7 @@
+from math import trunc
+num = float(input('Insira um valor: '))
+print('O valor digitado foi {} e sua porção inteira é {}.'.format(num, trunc(num)))
+
+# coisa de masqueico abaixo:
+numero = float(input('Insira um número: '))
+print('O valor digitado foi {} e sua porção inteira é {}.'.format(numero, int(numero)))
