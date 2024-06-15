691420ecc8e0a6f750e9578bfd6049cd8181a2d5
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 12:39:23 -0300

    Update exe014.py

--- Exercícios/exe014.py
+++ Exercícios/exe014.py
@@ -1,3 +1,3 @@
-c = int(input('Insira a temperatura atual em celsius para descobrir seu equivalente em fahrenheit: '))
+c = float(input('Insira a temperatura atual em celsius para descobrir seu equivalente em fahrenheit: '))
 f = c*9/5+32
 print('A temperatura {}°C equivale a {}°F.'.format(c, f))
