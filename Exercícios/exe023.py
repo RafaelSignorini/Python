c599328dd363db3318ba758398dcc1286d210525
Senhor_Ini <rafabs26126@gmail.com>
2024-07-05 11:08:25 -0300

    Create exe023.py

---
+++ Exercícios/exe023.py
@@ -0,0 +1,11 @@
+num = int(input('Insira um valor: '))
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+print('Abaixo estão as informações sobre o número {}:'.format(num))
+print('Unidade: {}'.format(unidade))
+print('Dezena: {}'.format(dezena))
+print('Centena: {}'.format(centena))
+print('Milhar: {}'.format(milhar))
