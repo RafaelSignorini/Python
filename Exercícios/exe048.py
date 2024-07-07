3f42a2d0f7b7021018e03619406ad402d4159735
Senhor_Ini <rafabs26126@gmail.com>
2024-07-07 15:58:06 -0300

    Update exe048.py

--- Exercícios/exe048.py
+++ Exercícios/exe048.py
@@ -1,7 +1,9 @@
 soma = 0
+cont = 0
 for a in range(1, 501):
     if a % 3 == 0:
         print(a, ', ', end='')
         soma += a
-print('\nA soma de todos estes valores é {}{}{}.'.format('\033[4;36m', soma, '\033[m'))
+        cont += 1
+print('\nA soma de todos os {}{}{} valores é {}{}{}.'.format('\033[7;35m', cont, '\033[m', '\033[4;36m', soma, '\033[m'))
 print('fim')
