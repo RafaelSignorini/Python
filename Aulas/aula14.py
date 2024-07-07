bd6a3015281bad3f358828c9ae4efa737a64bd7e
Senhor_Ini <rafabs26126@gmail.com>
2024-07-07 16:38:54 -0300

    Create aula14.py

---
+++ Aulas/aula14.py
@@ -0,0 +1,6 @@
+# Laço de repetição while (amo)
+# laço preferível pois não se limita a situações aonde é necessário saber um fim específico da condição da repetição
+
+# enquanto {condição} == False:
+#   acao
+# acao
