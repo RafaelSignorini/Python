26b6b6e82f6c50f3fb0eaa5ffa49554d7c75a370
Senhor_Ini <rafabs26126@gmail.com>
2024-07-07 16:15:42 -0300

    Update exe053.py

--- Exercícios/exe053.py
+++ Exercícios/exe053.py
@@ -1,1 +1,11 @@
-# palíndromo
+frase = str(input('Insira uma frase: ')).strip().lower()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print('O inverso de {} é {}.'.format(junto, inverso))
+if inverso == junto:
+    print('Essa frase é um palíndromo.')
+else:
+    print('Essa frase não é um palíndromo.')
