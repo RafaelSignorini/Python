57d5a0a66ad04f71412420b4a2946ad3d2c36048
Senhor_Ini <rafabs26126@gmail.com>
2024-07-07 15:34:18 -0300

    Update exe053.py

--- Exercícios/exe053.py
+++ Exercícios/exe053.py
@@ -1,7 +1,1 @@
-from datetime import date
-maiores = 0
-for a in range(0, 7):
-    ano = int(input('Insira seu ano de nascimento: '))
-    if date.today().year - ano >= 18:
-        maiores += 1
-print('No total há {}{}{} maiores de idade neste grupo.'.format('\033[7;34m', maiores, '\033[m'))
+# palíndromo
