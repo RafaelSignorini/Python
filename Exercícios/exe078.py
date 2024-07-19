6901ef5e50edcbae45ac795924f622b022c7b4ff
Senhor_Ini <rafabs26126@gmail.com>
2024-07-19 15:33:38 -0300

    Update exe078.py

--- Exercícios/exe078.py
+++ Exercícios/exe078.py
@@ -1,7 +1,7 @@
 valores = []
 posicaoMaior = posicaoMenor = 0
 for a in range(0, 5):
-    valores.append(int(input('Insira um valor na lista: ')))
+    valores.append(int(input(f'Insira um valor na lista para a posição {a}: ')))
     if a == 0:
         maior = valores[0]
         menor = valores[0]
@@ -12,5 +12,5 @@ for a in range(0, 5):
         menor = valores[-1]
         posicaoMenor = a
 print(f'Os valores registrados na lista foram: {valores}')
-print(f'O maior valor registrado foi {maior} e está na posição {posicaoMaior + 1}.')
-print(f'O menor valor registrado foi {menor} e está na posição {posicaoMenor + 1}.')
+print(f'O maior valor registrado foi {maior} e está na posição {posicaoMaior}.')
+print(f'O menor valor registrado foi {menor} e está na posição {posicaoMenor}.')
