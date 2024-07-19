3f9bf2797f57d311c05f31d26cfe557daa4bbc82
Senhor_Ini <rafabs26126@gmail.com>
2024-07-18 23:48:53 -0300

    Create exe072.py

---
+++ Exercícios/exe072.py
@@ -0,0 +1,11 @@
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 
+        'cinco', 'seis', 'sete', 'oito', 'nove', 
+        'dez', 'onze', 'doze', 'treze', 'quatorze', 
+        'quinze', 'dezesseis', 'dezessete', 'dezoito', 
+        'dezenove', 'vinte')
+while True:
+    num = int(input('Insira um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Erro, tente novamente.', end='')
+print(f'Você digitou o número {cont[num]}')
