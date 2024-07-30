b0f39f53a0a698bb2e1aa0c36d8c969454938edc
Senhor_Ini <rafabs26126@gmail.com>
2024-07-29 21:39:41 -0300

    Create exe092.py

---
+++ Exercícios/exe092.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+dados = {}
+dados['nome'] = str(input('Nome: ')).capitalize().strip()
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de Trabalho (0 caso não tenha): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=-' * 20)
+for k, v in dados.items():
+    print(f' - {k} = {v}')
