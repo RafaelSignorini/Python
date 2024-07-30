c77a94e5bb1a357de1a4cec003dee47e7211da5b
Senhor_Ini <rafabs26126@gmail.com>
2024-07-29 21:55:56 -0300

    Update exe093.py

--- Exercícios/exe093.py
+++ Exercícios/exe093.py
@@ -1,8 +1,15 @@
+from time import sleep
 jogador = {}
 partidas = []
 jogador['nome'] = str(input('Nome do Jogador: ')).strip().capitalize()
 total = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for a in range(0, total):
     partidas.append(int(input(f'Quantos gols foram feitos na {a+1}ª partida? ')))
+jogador['gols de cada partida'] = partidas[:]
+jogador['total de gols'] = sum(partidas)
+print('-=-' * 10)
 print(jogador)
-print(partidas)
+print('-=-' * 10)
+for k, v in jogador.items():
+    print(f'{k} = {v}')
+print('-=-' * 10)
