7f976f0e8a33998e28574d4db30b52415b0ccaef
Senhor_Ini <rafabs26126@gmail.com>
2024-08-10 12:43:07 -0300

    Update exe100.py

--- Exercícios/exe100.py
+++ Exercícios/exe100.py
@@ -1,1 +1,19 @@
 from random import randint
+nums = []
+def sorteia():
+    for a in range(1, 6):
+        num = randint(0, 9)
+        nums.append(num)
+    print(f'Os números gerados foram {nums}.')
+
+
+def somaPar():
+    pares = []
+    for a in range(1, len(nums)):
+        if nums[a] % 2 == 0:
+            pares.append(nums[a])
+    print(f'Os números pares registrados são {pares}.')
+
+
+sorteia()
+somaPar()
