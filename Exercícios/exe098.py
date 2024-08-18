139b552f3d80b75747d91c3f72b3fadb7685e072
Senhor_Ini <rafabs26126@gmail.com>
2024-08-18 09:19:36 -0300

    removi o laço 'while' que, aparentemente, era inútil

--- Exercícios/exe098.py
+++ Exercícios/exe098.py
@@ -1,15 +1,13 @@
 def contador(i, f, p):
-    while True:
-        print(f'De {i} até {f}, indo de {p} em {p}, temos: ')
-        for a in range(i, f+1, p):
-            print(f'{a}')
-        print(f'De {f} até {i}, indo de {p*2} em {p*2}, temos: ')
-        for a in range(f, i, p*-2):
-            print(f'{a}')
-        print(f'De {i*-1} até {f*3}, indo de {p*5} em {p*5}, temos: ')
-        for a in range(i*-1, (f+1)*3, p*5):
-            print(f'{a}')
-        break
+    print(f'De {i} até {f}, indo de {p} em {p}, temos: ')
+    for a in range(i, f+1, p):
+        print(f'{a}')
+    print(f'De {f} até {i}, indo de {p*2} em {p*2}, temos: ')
+    for a in range(f, i, p*-2):
+        print(f'{a}')
+    print(f'De {i*-1} até {f*3}, indo de {p*5} em {p*5}, temos: ')
+    for a in range(i*-1, (f+1)*3, p*5):
+        print(f'{a}')
     print('fim')
 
 
